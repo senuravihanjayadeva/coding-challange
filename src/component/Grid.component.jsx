7cfa52974020c1ecf328028d7a5af20609160fd3
senuravihanjayadeva <senurajayadeva@gmail.com>
2023-11-22 18:46:27 +0530

    Time calculation for setTimeout

--- src/component/Grid.component.jsx
+++ src/component/Grid.component.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import FadeLoader from 'react-spinners/FadeLoader';
 
 function Grid({ position, positionChange }) {
+  const [loading, setLoading] = useState(false);
 
   const teleportRobot = (x, y) => {
-    let xDistance = position.x - x;
-    let yDistance = position.y - y;
-    console.log(xDistance,yDistance)
+    console.time("teleport-time")
+    setLoading(true);
+    const totalDistance = Math.abs(position.x - x) + Math.abs(position.y - y);
     if (x >= 1 && x <= 5 && y >= 1 && y <= 5) {
-      setTimeout(()=>{
+      setTimeout(() => {
+        setLoading(false);
         positionChange({ x, y });
-      },2000)
+        console.timeEnd("teleport-time");
+      }, 1000 * totalDistance);
     }
   };
 
@@ -22,24 +26,26 @@ function Grid({ position, positionChange }) {
       row.push(
         <div
           key={`${x}-${y}`}
-          className={`cell ${
-            position.x === x && position.y === y ? "robot" : ""
-          }`}
-          onClick={() => teleportRobot(x, y)}
-        >
-        </div>
+          className={`cell ${position.x === x && position.y === y ? 'robot' : ''}`}
+          onClick={() => teleportRobot(x, y)}></div>
       );
     }
-    grid.push(
-      <div key={x}>
-        {row}
-      </div>
-    );
+    grid.push(<div key={x}>{row}</div>);
   }
 
   return (
     <div>
-      <div className="grid">{grid}</div>;
+      <div className="grid">
+        {grid}
+        <FadeLoader
+          color={'blue'}
+          loading={loading}
+          size={50}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+          className="loader-spinner"
+        />
+      </div>
     </div>
   );
 }
